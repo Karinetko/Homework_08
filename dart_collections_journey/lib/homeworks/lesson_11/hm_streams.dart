2a9d0320c669d3241b468958134893b99641074e
Karina Kononchuk <karinetko@icloud.com>
2025-07-15 19:21:45 +0300

    feat: Task 2 Streams

--- dart_collections_journey/lib/homeworks/lesson_11/hm_streams.dart
+++ dart_collections_journey/lib/homeworks/lesson_11/hm_streams.dart
@@ -1,1 +1,47 @@
+import 'dart:async';
 
+Future<void> streamFromIterable() async {
+  final stream = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
+
+  print('await for:');
+  await for (final value in stream) {
+    print(value);
+  }
+
+  final stream2 = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
+  print('listen:');
+  stream2.listen((value) => print(value));
+}
+
+Future<void> countdownStream() async {
+  final countdown = Stream<int>.periodic(
+    Duration(seconds: 1),
+    (x) => x + 1,
+  ).take(10);
+  await for (final number in countdown) {
+    print('$number...');
+  }
+}
+
+Future<void> streamControllerExample() async {
+  final controller = StreamController<String>();
+
+  controller.stream.listen(
+    (data) => print(data),
+    onDone: () => print('Стрім завершено'),
+  );
+
+  controller.sink.add('Hello');
+  controller.sink.add('World');
+  controller.sink.add('Dart');
+
+  await controller.close();
+}
+
+void main() async {
+  await streamFromIterable();
+
+  await countdownStream();
+
+  await streamControllerExample();
+}
